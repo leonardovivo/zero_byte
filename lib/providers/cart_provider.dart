94c20cada0d1b1edcb4cfec5250e8636cc5aa2e2
Leonardo Vivo Guerreiro <leonardovivo3@gmail.com>
2024-10-23 03:38:11 -0300

    Adicionado gerenciamento de quantidade de produtos no CartProvider

--- lib/providers/cart_provider.dart
+++ lib/providers/cart_provider.dart
@@ -10,23 +10,37 @@ class CartProvider with ChangeNotifier {
       0, (total, product) => total + product.price * product.quantity);
 
   void addProduct(Product product) {
-  var existingProduct = _products.firstWhere(
-    (p) => p.name == product.name,
-    orElse: () => Product(name: '', price: 0, quantity: 0),
-  );
-  
-  if (existingProduct.name.isNotEmpty) {
-    existingProduct.quantity++;
-  } else {
-    _products.add(product);
-  }
-  notifyListeners();
-}
+    var existingProduct = _products.firstWhere(
+      (p) => p.name == product.name,
+      orElse: () => Product(name: '', price: 0, quantity: 0),
+    );
 
+    if (existingProduct.name.isNotEmpty) {
+      existingProduct.quantity++;
+    } else {
+      _products.add(Product(
+        name: product.name,
+        price: product.price,
+        quantity: 1,
+      ));
+    }
+    notifyListeners();
+  }
 
   void removeProduct(Product product) {
-    _products.remove(product);
-    notifyListeners();
+    var existingProduct = _products.firstWhere(
+      (p) => p.name == product.name,
+      orElse: () => Product(name: '', price: 0, quantity: 0),
+    );
+
+    if (existingProduct.name.isNotEmpty) {
+      if (existingProduct.quantity > 1) {
+        existingProduct.quantity--;
+      } else {
+        _products.remove(existingProduct);
+      }
+      notifyListeners();
+    }
   }
 
   void clearCart() {
