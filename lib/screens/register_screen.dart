e89976908343e47d455af46b88c9e5e476dacbdf
Leonardo Vivo Guerreiro <leonardovivo3@gmail.com>
2024-10-25 04:35:35 -0300

    Validação dos campos para efetuar Cadastro

--- lib/screens/register_screen.dart
+++ lib/screens/register_screen.dart
@@ -1,8 +1,55 @@
 import 'package:flutter/material.dart';
 import 'package:zero_byte/widgets/banner.dart';
 
-class RegisterScreen extends StatelessWidget {
+class RegisterScreen extends StatefulWidget {
   const RegisterScreen({super.key});
+
+  @override
+  _RegisterScreenState createState() => _RegisterScreenState();
+}
+
+class _RegisterScreenState extends State<RegisterScreen> {
+  String registerMessage = '';
+  final String validUser = "user";
+  final String validEmail = 'teste@teste.com';
+  final String validPassword = '123456';
+
+  final TextEditingController userController = TextEditingController();
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  void _register() {
+    String user = userController.text;
+    String email = emailController.text;
+    String password = passwordController.text;
+
+    if (user.isEmpty || email.isEmpty || password.isEmpty) {
+      setState(() {
+        registerMessage = 'Todos os campos devem ser preenchidos!';
+      });
+    } else if (user == validUser &&
+        email == validEmail &&
+        password == validPassword) {
+      setState(() {
+        registerMessage = 'Cadastro bem sucedido!';
+      });
+      Future.delayed(const Duration(seconds: 1), () {
+        Navigator.pushReplacementNamed(context, '/home');
+      });
+    } else {
+      setState(() {
+        registerMessage = 'Dados inválidos! Tente novamente.';
+      });
+    }
+  }
+
+  @override
+  void dispose() {
+    userController.dispose();
+    emailController.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -30,7 +77,7 @@ class RegisterScreen extends StatelessWidget {
                 child: Padding(
                   padding: const EdgeInsets.only(bottom: 30),
                   child: Container(
-                    height: 600,
+                    height: 700,
                     width: 350,
                     decoration: BoxDecoration(
                       color: const Color.fromARGB(255, 38, 38, 38),
@@ -64,6 +111,23 @@ class RegisterScreen extends StatelessWidget {
                             ),
                           ),
                         ),
+                        const SizedBox(height: 10),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(
+                            registerMessage,
+                            style: TextStyle(
+                              color: registerMessage == 'Cadastro bem sucedido!'
+                                  ? Colors.green
+                                  : (registerMessage.isEmpty
+                                      ? Colors.red
+                                      : Colors.red),
+                              fontWeight: FontWeight.bold,
+                              fontFamily: "Poppins",
+                              fontSize: 15,
+                            ),
+                          ),
+                        ),
                         const SizedBox(height: 30),
                         const Text(
                           'Usuário',
@@ -89,8 +153,9 @@ class RegisterScreen extends StatelessWidget {
                             ),
                             child: ClipRRect(
                               borderRadius: BorderRadius.circular(40.0),
-                              child: const TextField(
-                                decoration: InputDecoration(
+                              child: TextField(
+                                controller: userController,
+                                decoration: const InputDecoration(
                                   labelText: 'Insira seu nome de Usuário...',
                                   fillColor: Color.fromARGB(255, 77, 76, 76),
                                   filled: true,
@@ -125,8 +190,9 @@ class RegisterScreen extends StatelessWidget {
                             ),
                             child: ClipRRect(
                               borderRadius: BorderRadius.circular(40.0),
-                              child: const TextField(
-                                decoration: InputDecoration(
+                              child: TextField(
+                                controller: emailController,
+                                decoration: const InputDecoration(
                                   labelText: 'Insira seu E-mail...',
                                   fillColor: Color.fromARGB(255, 77, 76, 76),
                                   filled: true,
@@ -161,9 +227,10 @@ class RegisterScreen extends StatelessWidget {
                             ),
                             child: ClipRRect(
                               borderRadius: BorderRadius.circular(40.0),
-                              child: const TextField(
+                              child: TextField(
+                                controller: passwordController,
                                 obscureText: true,
-                                decoration: InputDecoration(
+                                decoration: const InputDecoration(
                                   labelText: 'Insira sua Senha...',
                                   fillColor: Color.fromARGB(255, 77, 76, 76),
                                   filled: true,
@@ -175,7 +242,7 @@ class RegisterScreen extends StatelessWidget {
                         ),
                         const SizedBox(height: 35),
                         ElevatedButton(
-                          onPressed: () {},
+                          onPressed: _register,
                           style: ButtonStyle(
                             backgroundColor: WidgetStateProperty.all<Color>(
                               const Color.fromARGB(255, 0, 170, 255),
