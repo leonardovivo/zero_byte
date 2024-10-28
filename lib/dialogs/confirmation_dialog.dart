146e1ec175bad9d9a9ed8690353f91e5bd951b9e
Leonardo Vivo Guerreiro <leonardovivo3@gmail.com>
2024-10-28 02:35:00 -0300

    Super

--- lib/dialogs/confirmation_dialog.dart
+++ lib/dialogs/confirmation_dialog.dart
@@ -5,10 +5,10 @@ class ConfirmationDialog extends StatelessWidget {
   final VoidCallback onCancel;
 
   const ConfirmationDialog({
-    Key? key,
+    super.key,
     required this.onConfirm,
     required this.onCancel,
-  }) : super(key: key);
+  });
 
   @override
   Widget build(BuildContext context) {
